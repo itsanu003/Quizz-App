369cef6d664f90c99c9e25b812aa8de1e44c8651
itsanu003 <chauhanamit99368471@gmail.in>
2024-07-18 17:32:10 +0530

    Uploading initial Files

---
+++ src/assets/data.js
@@ -0,0 +1,82 @@
+export const data = [
+    {
+      question: "Which device is required for the Internet connection?",
+      option1: "Modem",
+      option2: "Router",
+      option3: "LAN Cable",
+      option4: "Pen Drive",
+      ans: 1,
+    },
+    {
+      question: "Which continent has the highest number of countries?",
+      option1: "Asia",
+      option2: "Europe",
+      option3: "North America",
+      option4: "Africa",
+      ans: 4,
+    },
+    {
+      question: "Junk e-mail is also called?",
+      option1: "Spam",
+      option2: "Fake",
+      option3: "Archived",
+      option4: "Bin",
+      ans: 1,
+    },
+    {
+      question: "A computer cannot BOOT if it does not have the?",
+      option1: "Application Software",
+      option2: "Internet",
+      option3: "Operating System",
+      option4: "Mouse",
+      ans: 3,
+    },
+    {
+      question: "First page of Website is termed as?",
+      option1: "Index Page",
+      option2: "Homepage",
+      option3: "Sitemap",
+      option4: "Pen Drive",
+      ans: 2,
+    },
+    {
+    question: "What does the acronym 'LAN' stand for?",
+    option1: "Local Access Network",
+    option2: "Long Area Network",
+    option3: "Local Area Network",
+    option4: "Long Access Network",
+    ans: 3
+  },
+  {
+    question: "Which of the following is a commonly used protocol for email transmission?",
+    option1: "HTTP",
+    option2: "SMTP",
+    option3: "FTP",
+    option4: "SNMP",
+    ans: 2
+  },
+  {
+    question: "Which layer of the OSI model is responsible for routing packets between devices?",
+    option1: "Transport Layer",
+    option2: "Network Layer",
+    option3: "Data Link Layer",
+    option4: "Physical Layer",
+    ans: 2
+  },
+  {
+    question: "What does DNS stand for?",
+    option1: "Dynamic Network System",
+    option2: "Domain Name System",
+    option3: "Digital Network Service",
+    option4: "Data Number Service",
+    ans: 2
+  },
+  {
+    question: "Which protocol is used to securely browse the web?",
+    option1: "FTP",
+    option2: "HTTP",
+    option3: "HTTPS",
+    option4: "SMTP",
+    ans: 3
+  },
+  ];
